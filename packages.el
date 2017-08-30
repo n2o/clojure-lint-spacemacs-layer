b9fc5215ba174665b58e1fa89795268e4b63a58f
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-30 09:29:39 +0200

    Rename layer

--- packages.el
+++ packages.el
@@ -1,16 +1,11 @@
 ;;; packages.el --- clojure-dev layer packages file for Spacemacs.
 
-(defconst clojure-dev-packages
+(defconst clojure-lint-packages
   '(flycheck
-    flycheck-joker
-    clojure-mode))
+    flycheck-joker))
 
-(defun clojure-dev/init-flycheck-joker ()
+(defun clojure-lint/init-flycheck-joker ()
   (use-package flycheck-joker))
 
-(defun clojure-dev/post-init-flycheck ()
+(defun clojure-lint/post-init-flycheck ()
   (add-hook 'clojure-mode-hook 'flycheck-mode))
-
-(defun clojure-dev/post-init-clojure-mode ()
-  (add-hook 'clojure-mode-hook 'turn-on-smartparens-strict-mode)
-  (global-set-key (kbd "C-k") 'sp-kill-sexp))
